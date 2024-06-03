f7258a5c973b2cd17a042882761e7a36330ed966
swaggimus_grime <hunter66888@gmail.com>
2024-06-03 19:23:32 -0400

    Add dining room

--- Deference/res/shaders/screen.vs.hlsl
+++
@@ -1,9 +0,0 @@
-#include "Vertex.hlsli"
-
-ScreenVertexOut main(float3 pos : POSITION, float2 tex : TEXCOORD) 
-{
-    ScreenVertexOut v;
-    v.pos = float4(pos, 1.f);
-    v.tex = tex;
-    return v;
-}
